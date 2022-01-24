6132cf3148c74ac82acfb1d09aa516b6a3164548
lucas cruz <lucas_cruz100@live.com>
2022-01-24 08:47:46 -0300

    Novos atributos adicionados

--- spec/factories/usuarios.rb
+++ spec/factories/usuarios.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :usuarios, class: 'ApiNoticia::Models::Usuario' do
     nome { Faker::Name.first_name }
     email { Faker::Internet.email }
-    password_digest { Faker::Number.number(digits: 6) }
+    password { Faker::Internet.password }
+    biografia { Faker::Lorem.sentence }
+    ativo { true }
   end
 end
